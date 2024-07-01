f13b0afc343ccbc686e3102bec64b0899914e19b
Lyn <0xlynett@proton.me>
2024-07-01 13:54:02 +0000

    change meta description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Etherpedia";
-export const SITE_DESCRIPTION = "A project by 2077 Collective";
+export const SITE_DESCRIPTION =
+  "The guide to all things Ethereum. A project by 2077 Collective.";
